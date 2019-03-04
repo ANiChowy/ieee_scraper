44ad69951b88e75e028417f84d00ca3e2fac64a8
Scott <2251386+PolarisScientia@users.noreply.github.com>
2019-03-04 14:03:56 -0500

    Author(s), Date scraping added & Readme update

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 IEEE Xplore Digital Library Conference abstract scraping python script
 
 # Dependencies
-- Selenium
+- Selenium (& Webdriver)
 - lxml
 - xlsxwriter
+- cssselector
 
 Please make sure to have the appropriate webdriver for Selenium.
 
@@ -13,9 +14,12 @@ Please make sure to have the appropriate webdriver for Selenium.
 pip install selenium
 pip install lxml
 pip install xlsxwriter
+pip install cssselector
 ... (install webdriver) ...
 scraper.py
 ```
+
+You may need to change webdriver if you aren't using firefox.
 
 Change line 7 to match the URL you are trying to scrape. You may need to add more rows with the tag &rowsPerPage=(NumElements).
 
--- scraper.py
+++ scraper.py
@@ -21,6 +21,8 @@ worksheet = workbook.add_worksheet()
 bold = workbook.add_format({'bold': True})
 worksheet.write('A1', 'Title', bold)
 worksheet.write('B1', 'Abstract', bold)
+worksheet.write('C1', 'Authors', bold)
+worksheet.write('D1', 'Date of Conference', bold)
 for elem in htmlElem.iterlinks():
     link = elem[2]
     if link.startswith("/document/") and link.endswith("/") and link not in list_of_links:
@@ -46,6 +48,26 @@ for elem in htmlElem.iterlinks():
             aText = elem.text_content()
             print(aText)
             worksheet.write(row, col+1, aText)
+        num_authors = nhtmlElem.cssselect(".authors-count")
+        if num_authors:
+            num_authors = num_authors[0].text_content()
+            authors = []
+            print('Authors(' + str(num_authors).strip() + '): ', end='')
+            for i in range(1, int(num_authors)+1):
+                aName = nhtmlElem.cssselect("span.authors-info:nth-child(" + str(i) + ") > span:nth-child(1) > a:nth-child(1) > span:nth-child(1)")
+                #There is a better way to loop through this list but I can't be bothered.
+                #It may cause issues when number of authors is large and not fully displayed
+                #on the screen. Will need to test...
+                aName = aName[0].text_content()
+                authors.append(aName)
+            print(','.join(map(str, authors)))
+            worksheet.write(row, col+2, ','.join(map(str, authors)))
+            print('')
+        date = nhtmlElem.cssselect(".doc-abstract-confdate")
+        if date and len(date) > 1:
+            date = date[1]
+            print(date)
+            worksheet.write(row, col+3, date)
         row += 1
         #browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
 
